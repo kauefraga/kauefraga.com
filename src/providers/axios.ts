dda1ecefbbd17777269a10c21a80bcc11cbffc1f
kauefraga <kauefragarodrigues456@gmail.com>
2022-09-02 00:41:23 -0300

    :adhesive_bandage: fix: `baseURL` error

--- src/providers/axios.ts
+++ src/providers/axios.ts
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export const api = axios.create({
   baseURL: process.env.DEV
-    ? "https://kauefraga.vercel.app/api"
-    : "http://localhost:3000/api/v1",
+    ? "http://localhost:3000/api/v1"
+    : "https://kauefraga.vercel.app/api/v1",
 });
