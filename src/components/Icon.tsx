ff4301ab8e8469721b890c7762fe102e42895d19
kauefraga <kauefragarodrigues456@gmail.com>
2022-09-02 00:18:30 -0300

    :adhesive_bandage: fix: add <Icon> to fix linting error

---
+++ src/components/Icon.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+
+type ImageExtensions = ".png" | ".jpg" | ".jpeg" | "";
+
+interface IconProps {
+  src: `/${string}${ImageExtensions}`;
+  alt: string;
+}
+
+export const Icon: React.FC<IconProps> = ({ src, alt }) => {
+  return (
+    <div className="justify-items-center w-36 h-36 rounded-full">
+      <Image
+        src={src}
+        alt={alt}
+        width={184}
+        height={184}
+        className="rounded-full"
+        placeholder="blur"
+        blurDataURL="loading..."
+      />
+    </div>
+  );
+};
