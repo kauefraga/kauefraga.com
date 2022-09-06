bc5caf7544b90978ab31a7d57ba77c9146455ded
kauefraga <kauefragarodrigues456@gmail.com>
2022-09-05 21:01:04 -0300

    :wrench: config: use `env.NODE_ENV` instead of new .env.DEV

--- src/providers/axios.ts
+++ src/providers/axios.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: process.env.DEV
-    ? "http://localhost:3000/api/v1"
-    : "https://kauefraga.vercel.app/api/v1",
+  baseURL:
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:3000/api/v1"
+      : "https://kauefraga.vercel.app/api/v1",
 });
