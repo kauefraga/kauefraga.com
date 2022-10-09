cb1b58ce9341d7f651f3aa8bb69dc7721dc85bef
kauefraga <kauefragarodrigues456@gmail.com>
2022-10-09 17:13:43 -0300

    :recycle: chore: just linting

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,15 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>Kaue Fraga Rodrigues</title>
+        <link rel="icon" type="image/png" href="https://github.com/kauefraga.png" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
-
-export default MyApp
