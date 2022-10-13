3a1fc2b3c0a0341db2b15de97e028d996fd55487
kauefraga <kauefragarodrigues456@gmail.com>
2022-10-13 16:52:44 -0300

    :recycle: chore: add interface into services instead pages

--- src/services/getRandomPhotos.ts
+++ src/services/getRandomPhotos.ts
@@ -1,7 +1,15 @@
 import axios from 'axios';
 
+export interface IPhoto {
+  id: number;
+  author: string;
+  width: number;
+  height: number;
+  download_url: string;
+}
+
 export const getRandomPhotos = async () => {
-  const response = await axios.get('https://picsum.photos/v2/list')
+  const response = await axios.get<IPhoto[]>('https://picsum.photos/v2/list')
     .then(res => res.data);
 
   return response;
--- src/services/getRepos.ts
+++ src/services/getRepos.ts
@@ -1,7 +1,14 @@
 import axios from 'axios';
 
+export interface IRepo {
+  id: number;
+  name: string,
+  description: string,
+  html_url: string,
+}
+
 export const getRepos = async () => {
-  const response = await axios.get('https://api.github.com/users/kauefraga/repos')
+  const response = await axios.get<IRepo[]>('https://api.github.com/users/kauefraga/repos')
     .then(res => res.data);
 
   return response;
