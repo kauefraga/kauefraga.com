b760cd4b298e8ab5c774028bb3f0762b5c671256
Kauê Fraga Rodrigues <88486000+kauefraga@users.noreply.github.com>
2023-05-18 21:34:38 -0300

    :sparkles: feat: remove google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,6 @@
-/* eslint-disable @next/next/no-script-component-in-head */
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
-import Script from 'next/script';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -10,8 +8,6 @@ export default function MyApp({ Component, pageProps }: AppProps) {
       <Head>
         <title>Kaue Fraga Rodrigues</title>
         <link rel="icon" type="image/png" href="https://github.com/kauefraga.png" />
-        <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8455364319195819"
-     crossOrigin="anonymous"></Script>
       </Head>
       <Component {...pageProps} />
     </>
